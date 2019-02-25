ada03cefcb4346734b74eb43610f8f12391f6424
Olegas Kosuchinas <olegas.kosuchinas@gmail.com>
2019-02-25 02:58:40 +0100

    some styling for feedback

--- client/src/components/feedback/Error.js
+++ client/src/components/feedback/Error.js
@@ -1,3 +1,4 @@
 import React from 'react'
+import styles from './Error.css'
 
-export default ({ children }) => <div>{children}</div>
+export default ({ children }) => <div className={styles.error}>{children}</div>
